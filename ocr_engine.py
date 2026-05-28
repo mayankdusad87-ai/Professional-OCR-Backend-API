fcc4a409ab922ca1a120b85e6d300f1ba300cd8a
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-05-28 13:51:48 +0530

    Update ocr_engine.py

--- ocr_engine.py
+++ ocr_engine.py
@@ -1,1 +1,17 @@
+from paddleocr import PaddleOCR
 
+ocr = PaddleOCR(
+    use_angle_cls=True,
+    lang='en'
+)
+
+def extract_text(image_path):
+
+    result = ocr.ocr(image_path, cls=True)
+
+    text = ""
+
+    for line in result[0]:
+        text += line[1][0] + "\n"
+
+    return text
